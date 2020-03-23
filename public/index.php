d0f80d6866d9eb2c3c1a549bfbefc7bccedb5198
unknown <exodemandah@gmail.com>
2020-03-23 10:22:09 +0100

    modif du css

--- public/index.php
+++ public/index.php
@@ -31,38 +31,36 @@
 
          <div id="conteneur">
 
-<div class="card" style="width: 18rem;">
+<div class="card" style="width: 15rem;">
   <img src="img/lyon.jpg" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Lyon</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <button name="button">Acheter</button>
+    <button name="button">visiter</button>
   </div>
 </div>
 
-<div class="card" style="width: 18rem;">
+<div class="card" style="width: 15rem;">
   <img src="img/marseille2.jpg" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Marseille</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <button name="button">Acheter</button>
+    <button name="button">visiter</button>
   </div>
 </div>
 
 
-<div class="card" style="width: 18rem;">
+<div class="card" style="width: 15rem;">
   <img src="img/paris.jpg" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Paris</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <button name="button">Acheter</button>
+    <button name="button">visiter</button>
   </div>
 </div>
 
 </div>
 
-
-   
     </main>
 
     <footer>
