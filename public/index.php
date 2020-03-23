a2624473ec1e5681b8d81bf95cd911edb1995d05
unknown <exodemandah@gmail.com>
2020-03-23 16:47:49 +0100

    dernière modif

--- public/index.php
+++ public/index.php
@@ -2,14 +2,17 @@
 $title = "Accueil";
 require_once '../layout/header.php';
 
-?>
-
-		
+?>	
 		
 	</header>
+
+
     
-    
-		<img src="img/bk.jpg">
+<div class=" overlay-image _bo "><a href="URL_LIEN">
+ <img class=" image _bp " src="/img/bk.jpg" alt="Alt text" />
+ <div class=" text _q ">Trouver le meilleur endroit pour vivre</div>
+</a></div>
+	
 		<main>
 
     	<h1>Trouver ici la solution pour vous loger</h1>
@@ -50,7 +53,7 @@ require_once '../layout/header.php';
     </main>
 
     <h2>Ils nous ont fait confiance...</h2><br/>
-
+ <div id="container">
    	<section>
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">    
 		<i class="fas fa-star"></i>
@@ -78,6 +81,8 @@ require_once '../layout/header.php';
 		<h3>Joshua</h3>
 		<q>Tout était parfait. Accueil et confort de la chambre ,chambre plus grande que prévue merci à la direction et au Corona ? Lit assez haut mais pratique et confortable</q>
 	</section>
+
+	</div>
 
 
 <?php require_once '../layout/footer.php'; ?>
