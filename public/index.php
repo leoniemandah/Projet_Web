c0acf2ca3424f9d402e4ce175f7968db0681e563
Mareva Gauss <mareva.gauss@ynov.com>
2020-03-23 15:34:29 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -51,8 +51,6 @@ require_once '../layout/header.php';
 
     <h2>Ils nous ont fait confiance...</h2><br/>
 
-   	<div id="container">
-
    	<section>
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">    
 		<i class="fas fa-star"></i>
@@ -78,8 +76,6 @@ require_once '../layout/header.php';
 		<h3>Joshua</h3>
 		<q>Tout était parfait. Accueil et confort de la chambre ,chambre plus grande que prévue merci à la direction et au Corona ? Lit assez haut mais pratique et confortable</q>
 	</section>
-	</div>
 
-</div>
 
 <?php require_once '../layout/footer.php'; ?>
