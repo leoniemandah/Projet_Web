aa6bac2caa6d203cdc05bcbe5fb0683ebb6e9249
unknown <exodemandah@gmail.com>
2020-03-26 09:50:23 +0100

    modification du formulaire d'inscription

--- public/host.php
+++ public/host.php
@@ -7,39 +7,36 @@ require_once '../layout/header.php';
 
 </header>
 
-<h2>Formulaire d'inscription</h2><br/>
-<form method="POST">
-  <p><i>Complétez le formulaire. Les champs marqué par </i><em>*</em> sont <em>obligatoires</em></p>
-  <fieldset>
-    <legend>Contact</legend>
-      <label for="nom">Nom <em>*</em></label>
-
-      <input id="nom" placeholder="Leonie Gauss" autofocus="" required=""><br>
-      <label for="telephone">Portable</label>
+<body>
+  <div class="container">  
+    <form id="contact" action="" method="post">
+    
+      <h2>Formulaire d'inscription</h2>
       
-      <input id="telephone" type="tel" placeholder="06xxxxxxxx" pattern="06[0-9]{8}"><br>
-      <label for="email">Email <em>*</em></label>
-      <input id="email" type="email" placeholder="prenomnom@gmail.com" required="" pattern="[a-zA-Z]*[a-zA-Z]*@gmail.com"><br>
-  </fieldset>
-  <fieldset>
-    <legend>Information personnelles</legend>
-      <label for="age">Age<em>*</em></label>
-      
-      <input id="age" type="number" placeholder="xx" pattern="[0-9]{2}" required=""><br>
-      <label for="sexe">Sexe</label>
+      <fieldset>
+        <input placeholder="Nom et prénom" type="text" tabindex="1" required autofocus>
+      </fieldset>
+      <fieldset>
+        <input placeholder="Email" type="email" tabindex="2" required>
+      </fieldset>
+      <fieldset>
+        <input placeholder="Numéro de téléphone (facultatif)" type="tel" tabindex="3" required>
+      </fieldset>
       <select id="sexe">
         <option value="F" name="sexe">Femme</option>
         <option value="H" name="sexe">Homme</option>
       </select><br>
-      <label for="comments">Pourquoi voulez-vous devenir host?</label>
-      <textarea id="comments"></textarea>
-  </fieldset>
- 
-  
-  <p><input type="submit" value="Soummettre"></p>
-</form>
+      <fieldset>
+        <textarea placeholder="Pourquoi voulez-vous devenir host?...." tabindex="5" required></textarea>
+      </fieldset>
+      <fieldset>
+        <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Envoyer</button>
+      </fieldset>
+    </form>
+  </div>
 
 
+  </body>
 
 
 <?php require_once '../layout/footer.php'; ?>
